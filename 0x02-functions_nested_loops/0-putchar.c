04088a5366f545b3652e9db6cd946874911c8e5c
ahmedJo3ran <ar1818a@gmail.com>
2023-06-21 09:37:16 -0700

    fddsfs

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * main - Entry point
+ *
+ * Decription : what number is greater or less
+ *
+ * Return: Always 0 (Success)
+*/
+
+int main(void)
+{
+	char str[] = "_putchar";
+	int n;
+
+	for (n = 0; n < 8; n++)
+	{
+		_putchar(str[n]);
+	}
+
+	_putchar('\n');
+
+	return (0);
+}
---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * print_alphabet - to print lower case alphatets
+ *
+ * Return : void
+*/
+
+void print_alphabet(void)
+{
+	char a = 'a';
+
+	for (a = 'a'; a <= 'z'; a++)
+		_putchar(a);
+
+	_putchar('\n');
+}
