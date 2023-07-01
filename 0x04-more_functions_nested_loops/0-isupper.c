76502c1bade2adc220f3ef4d5d66aac84817bc4b
ahmedJo3ran <ar1818a@gmail.com>
2023-06-30 23:52:19 -0700

    jjggg

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _isupper - checks if a character is uppercase
+ * @c: the character to be checked
+ *
+ * Return: 1 if the character is uppercase, 0 otherwise
+*/
+
+int _isupper(int c)
+{
+	if (c >= 65 && c  <= 90)
+	{
+		return (1);
+	}
+	return (0);
+}
---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,10 @@
+#include "main.h"
+void more_numbers(void)
+{
+	int x, y;
+
+	for (x = 0; x < 10; x++)
+	{
+		for (y = 0; y <= 14;
+	}
+}
